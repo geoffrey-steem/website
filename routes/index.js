ac0f26ee57bad6797df35103c6171057ed9d25e3
geoffrey <@geoffrey>
2016-08-16 18:55:26 +0000

    Added redirection for h-index

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,61 @@ router.get('/', function(req, res, next) {
 
 /* GET steemit-estimator. */
 router.get('/steemit-estimator', function(req, res, next) {
-    //res.redirect('http://steemit-estimator.herokuapp.com');
     res.writeHead(301,
       {Location: 'http://steemit-estimator.herokuapp.com'}
     );
     res.end();
 });
+
+/* GET steem-index. */
+router.get('/steem-index', function(req, res, next) {
+    res.writeHead(301,
+      {Location: 'http://seed.zapto.org:5001/'}
+    );
+    res.end();
+});
+
+/* GET steem-index-full. */
+router.get('/steem-index/full/:id', function(req, res, next) {
+    res.writeHead(301,
+      {Location: 'http://seed.zapto.org:5001/full/' + req.params.id}
+    );
+    res.end();
+});
+
+
+/* GET steem-index-quick. */
+router.get('/steem-index/:id', function(req, res, next) {
+    res.writeHead(301,
+      {Location: 'http://seed.zapto.org:5001/' + req.params.id}
+    );
+    res.end();
+});
+
+/* GET steem-index. */
+router.get('/hindex', function(req, res, next) {
+    res.writeHead(301,
+      {Location: 'http://seed.zapto.org:5001/'}
+    );
+    res.end();
+});
+
+/* GET steem-index-full. */
+router.get('/hindex/full/:id', function(req, res, next) {
+    res.writeHead(301,
+      {Location: 'http://seed.zapto.org:5001/full/' + req.params.id}
+    );
+    res.end();
+});
+
+/* GET steem-index-quick. */
+router.get('/hindex/:id', function(req, res, next) {
+    res.writeHead(301,
+      {Location: 'http://seed.zapto.org:5001/' + req.params.id}
+    );
+    res.end();
+});
+
 
 /* Get account. */
 router.get('/:name', function(req, res, next) {
